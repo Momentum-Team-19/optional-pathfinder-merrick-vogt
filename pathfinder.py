3328088da1e047a2947cf9e671c18b3c206f252e
Nathan Vogt <merrick.vogt@gmail.com>
2023-08-09 16:17:53 -0400

    correct paths. need to find shortest path

--- pathfinder.py
+++ pathfinder.py
@@ -44,67 +44,67 @@ def create_2d_array_from_list(data_points, rows, columns):
 
 
 def greedy_walk(data_array, rows, columns):
-    y = 300
-    # start path at [0, y]
-    short_path = [[0, y]]
-    # for x in range(columns-1):
-    for x in range(250):
-        current_elevation = data_array[x][y]
-        # bounds for top of the map
-        upright_elevation = data_array[x+1][min(y+1, columns-1)]
-        right_elevation = data_array[x+1][y]
-        # bounds for bottom of the map
-        downright_elevation = data_array[x+1][max(y-1, 0)]
+    short_path = []
+    shortest_path = []
+    for y in range(rows-1):
+        # restart elevation change count for each path
+        total_elevation_change = 0
 
-        options = [abs(current_elevation - upright_elevation),
-                   abs(current_elevation - right_elevation),
-                   abs(current_elevation - downright_elevation)]
+        # for x in range(columns-1):
+        for x in range(columns-1):
+            current_elevation = data_array[y][x]
+            # bounds for top of the map. min(y+1, columns-1)
+            upright_elevation = data_array[max(y-1, 0)][x+1]
+            right_elevation = data_array[y][x+1]
+            # bounds for bottom of the map. max(y-1, 0)
+            downright_elevation = data_array[min(y+1, columns-1)][x+1]
 
-        # logic for finding least elevation change of 3 options
-        # if all options are unique
-        if options[0] != options[1] and options[0] != options[2] and options[1] != options[2]:
-            greedy_option = min(options)
-        # if all options are equal
-        elif options[0] == options[1] and options[1] == options[2]:
-            greedy_option = options[1]
+            options = [abs(current_elevation - upright_elevation),
+                       abs(current_elevation - right_elevation),
+                       abs(current_elevation - downright_elevation)]
+        
+            # Default to an invalid index
+            greedy_option_index = 1
 
-        # if only option 0 equals option 1
-        elif options[0] == options[1]:
-            greedy_option = options[1]
-            if options[1] > options[2]:
-                greedy_option = options[2]
+            # If all options are distinct
+            if options[0] != options[1] and options[0] != options[2] and options[1] != options[2]:
+                greedy_option_index = options.index(min(options))
+            # If all options are equal
+            elif options[0] == options[1] and options[1] == options[2]:
+                greedy_option_index = 1
+            # If only option 0 equals option 1
+            elif options[0] == options[1]:
+                greedy_option_index = 1
+                if options[1] > options[2]:
+                    greedy_option_index = 2
+            # If only option 2 equals option 1
+            elif options[1] == options[2]:
+                greedy_option_index = 1
+                if options[1] > options[0]:
+                    greedy_option_index = 0
+            # If options 0 equals option 2
+            elif options[0] == options[2]:
+                greedy_option_index = 0 if random.choice([0, 2]) == 0 else 2
+                if options[1] < options[0]:
+                    greedy_option_index = 1
 
-        # if only option 2 equals option 1
-        elif options[1] == options[2]:
-            greedy_option = options[1]
-            if options[1] > options[0]:
-                greedy_option = options[0]
+            # Move to greedy option
+            if greedy_option_index == 0:
+                y += -1
+            elif greedy_option_index == 1:
+                y += 0
+            elif greedy_option_index == 2:
+                y += 1
 
-        # if options 0 equals option 2
-        elif options[0] == options[2]:
-            greedy_option = random.choice([options[0], options[2]])
-            if options[1] < options[0]:
-                greedy_option = options[1]
-
-        # move to greedy option
-        if greedy_option == options[0]:
-            y += 1
-        elif greedy_option == options[1]:
-            y += 0
-        elif greedy_option == options[2]:
-            y += -1
-
-        if x < 11:
-            print(f'current_elevation: {current_elevation}')
-            print(f'up: {upright_elevation} right: {right_elevation} down: {downright_elevation}')
-            print(options)
-            print(f'option picked: {greedy_option}')
-            print(f'x: {x+1} y: {y}')
-
-        # start appending at index 1 instead of 0.
-        short_path.append([x+1, y])
-
-    return short_path
+            # start appending at index 1 instead of 0.
+            short_path.append([x+1, y])
+            
+            total_elevation_change += options[greedy_option_index]
+        shortest_path.append(total_elevation_change)
+        
+    shortest_path_index = shortest_path.index(min(shortest_path))
+    print(f'shortest path index: {shortest_path_index}')
+    return short_path, shortest_path_index
 
 
 def draw_short_path(text_file_path, input_img_path, output_img_path):
@@ -131,15 +131,23 @@ def draw_short_path(text_file_path, input_img_path, output_img_path):
 
     # Using Greedy Algorithm
     data_array = create_2d_array_from_list(data_points, rows, columns)
-    # print(data_array)
-    greedy_walk_array = greedy_walk(data_array, rows, columns)
     
-    # print(greedy_walk_array)
+    greedy_walk_paths = greedy_walk(data_array, rows, columns)
+    # print(greedy_walk_paths)
 
-    for index, location in enumerate(greedy_walk_array):
-        pixels[location[0], location[1]] = (255, 0, 0)  # set pixel to red
-        if index > 130:
+    for index, location in enumerate(greedy_walk_paths[0]):
+        
+        if greedy_walk_paths[1]*(columns-1) <= index < 2*greedy_walk_paths[1]*(columns-1):
+            print(f'green: {index} location: {location}')
             pixels[location[0], location[1]] = (0, 255, 0)  # set pixel to green
+        else:
+            pixels[location[0], location[1]] = (255, 0, 0)  # set pixel to red
+
+    print(greedy_walk_paths[0])
+    print(greedy_walk_paths[0][1])
+    # if index == greedy_walk_paths[1]:
+    #         pixels[location[0], location[1]] = (0, 255, 0)  # set pixel to green
+    
     # Save the modified image
     image.save(output_img_path)
 
@@ -169,13 +177,13 @@ def create_img_from_elevation(text_file_path, output_image_path):
     pixels = image.load()
     # Loop through each data point with index and element
     for i, data_point in enumerate(data_points):
-        # Clever way of getting the x-index with using the 
-        # modulo of the image_size so it sets to zero after 
+        # Clever way of getting the x-index with using the
+        # modulo of the image_size so it sets to zero after
         # moving on to the next line below
         x = i % image_size
 
-        # Clever way of getting the y-index by dividing the 
-        # index by the image size and rounding down. 
+        # Clever way of getting the y-index by dividing the
+        # index by the image size and rounding down.
         y = i // image_size
 
         # use the function to get the grayscale value
@@ -185,7 +193,7 @@ def create_img_from_elevation(text_file_path, output_image_path):
 
         # Show processing progress
         # if x % image_size - 1 == 0 and y % 10 == 0:
-        #     print(f"Finished processing line {y + 1} out of {image_size}")
+        # print(f"Finished processing line {y + 1} out of {image_size}")
 
     # Apply contrast stretching to the image
     image = contrast_stretch(image)
@@ -202,11 +210,16 @@ create_img_from_elevation(small_input_file, small_output_image)
 small_one_path_output_image = 'small_one_path_output_image.png'
 draw_short_path(small_input_file, small_output_image, small_one_path_output_image)
 
-large_input_file = 'elevation_large.txt'
-large_output_image = 'large_elevation_image.png'
-create_img_from_elevation(large_input_file, large_output_image)
+# large_input_file = 'elevation_large.txt'
+# large_output_image = 'large_elevation_image.png'
+# create_img_from_elevation(large_input_file, large_output_image)
 # short_path_output_image = 'short_path_output_image.png'
 # draw_short_path(large_input_file, large_output_image, short_path_output_image)
 
 # Testing
 
+test_input_file = 'test.txt'
+test_output_image = 'test_image.png'
+create_img_from_elevation(test_input_file, test_output_image)
+test_one_path_output_image = 'test_one_path_output_image.png'
+draw_short_path(test_input_file, test_output_image, test_one_path_output_image)
